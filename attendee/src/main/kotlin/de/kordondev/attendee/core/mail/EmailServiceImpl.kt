08a8521b4d983af050fb250504e640bbcf4f5a24
Arne Maier <Arne_Maier@gmx.de>
2020-05-19 17:30:21 +0200

    Add config for not sending mail

--- attendee/src/main/kotlin/de/kordondev/attendee/core/mail/EmailServiceImpl.kt
+++ attendee/src/main/kotlin/de/kordondev/attendee/core/mail/EmailServiceImpl.kt
@@ -3,13 +3,19 @@ package de.kordondev.attendee.core.mail
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 import javax.mail.SendFailedException
 
+@Configuration
 @Service("EmailService")
-class EmailServiceImpl {
+class EmailServiceImpl (
+        @Value("\${application.mail.send}") private val sendMail: Boolean,
+        @Value("\${spring.mail.from}") private val sendFrom: String
+) {
 
     @Autowired
     lateinit var emailSender: JavaMailSender
@@ -18,11 +24,17 @@ class EmailServiceImpl {
     fun sendSimpleMessage(to: String, subject: String, text: String) {
         try {
             val message = SimpleMailMessage()
+            message.setFrom(sendFrom)
             message.setTo(to)
             message.setSubject(subject)
             message.setText(text)
-            emailSender.send(message)
-            logger.info("Mail send")
+            if (sendMail) {
+                emailSender.send(message)
+                logger.info("Mail send to $to", to);
+            } else {
+                logger.info("Could have send message: $message", message)
+            }
+
         } catch (exception:SendFailedException) {
             logger.error(exception.message)
         }
