2a00a594bd6583b86bde952bbeb09f81522dc841
Arne Maier <Arne_Maier@gmx.de>
2020-08-02 21:42:47 +0200

    Add normalize css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="wrapper">
+  <div class="one">One</div>
+  <div class="two">Two</div>
+  <div class="three">Two</div>
+  </div>
+    <Header />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 };
 </script>
@@ -23,6 +30,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) minmax(auto, 1000px) minmax(0, 1fr);
+  grid-gap: 14px;
+}
+
+.one {
+  grid-column: 1;
+  grid-row: 1;
+  background-color: red;
+}
+.two {
+  grid-column: 2;
+  grid-row: 1;
+  background-color: yellowgreen;
+}
+.three {
+  grid-column: 3;
+  grid-row: 1;
+  background-color: hotpink;
 }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <img alt="Zeltlager logo" class="hero-image" src="../assets/logo.png" />
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.hero-image {
+  max-width: 1000px;
+  width: 100%;
+}
+
+</style>
