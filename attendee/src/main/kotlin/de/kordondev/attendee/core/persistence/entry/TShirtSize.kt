4c2e168146faabda91167f698060f729325d57ea
Arne Maier <Arne_Maier@gmx.de>
2022-02-27 21:27:07 +0100

    Use correct tshirt sizes

--- attendee/src/main/kotlin/de/kordondev/attendee/core/persistence/entry/TShirtSize.kt
+++ attendee/src/main/kotlin/de/kordondev/attendee/core/persistence/entry/TShirtSize.kt
@@ -1,22 +1,32 @@
 package de.kordondev.attendee.core.persistence.entry
 
+import de.kordondev.attendee.exception.BadRequestException
+
 enum class TShirtSize(val size: String) {
-    ONE_HUNDRED_TWENTY_EIGHT("128"),
-    ONE_HUNDRED_FORTY("140"),
-    ONE_HUNDRED_FIFTY_TWO("152"),
-    ONE_HUNDRED_FIFTY_EIGHT("158"),
-    ONE_HUNDRED_SIXTY_FOUR("164"),
+    S104110("104/110"),
+    S116128("116/128"),
+    S140152("140/152"),
+    S164("164"),
+    S176("176"),
     S("S"),
     M("M"),
     L("L"),
     XL("XL"),
     XXL("XXL"),
-    XXXL("XXXL"),
-    XXXXL("XXXXL"),
-    XXXXXL("XXXXXL");
+    X3L("3XL"),
+    X4L("4XL"),
+    X5L("5XL");
+
     companion object {
-        private val map = TShirtSize.values().associateBy(TShirtSize::size)
-        fun fromString(size: String) = map[size]
+        fun fromString(size: String): TShirtSize {
+            return values().find { it.size == size }
+                ?: throw BadRequestException("TShirtSize (${size}) ist kein valider Wert")
+        }
+    }
+
+    override
+    fun toString(): String {
+        return size
     }
 }
 
