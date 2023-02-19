3780068313ecb916af690d50e916e25f2ab30f4e
Arne Maier <Arne_Maier@gmx.de>
2023-02-19 15:49:45 +0100

    Add endpoint to change user role

---
+++ attendee/src/main/kotlin/de/kordondev/attendee/rest/model/request/RestUserRoleRequest.kt
@@ -0,0 +1,9 @@
+package de.kordondev.attendee.rest.model.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+
+data class RestUserRoleRequest(
+    @field:Pattern(regexp = "^(USER|SPECIALIZED_FIELD_DIRECTOR)$",message = "role needs to be one of USER or SPECIALIZED_FIELD_DIRECTOR")
+    val role: String
+)
