ce4ed667691f1fb1ec74ce6ae4a5c54f370d3b71
Arne Maier <arne.maier@inovex.de>
2023-03-11 13:56:40 +0100

    Add more tests

--- attendee/src/test/kotlin/de/kordondev/attendee/core/pdf/PDFHelperTest.kt
+++ attendee/src/test/kotlin/de/kordondev/attendee/core/pdf/PDFHelperTest.kt
@@ -25,12 +25,12 @@ class PDFHelperTest {
     @Test
     fun getOptimizedLeaderAndAttendees() {
         val allAttendees = listOf(
-            createAttendee(1L, "1995-05-01"),
-            createAttendee(2L, "1996-03-01"),
-            createAttendee(6L, "1990-03-01"),
-            createAttendee(3L, "2005-05-01"),
-            createAttendee(4L, "2011-05-01"),
-            createAttendee(5L, "2022-05-01"),
+            createAttendee(1L, "1995-05-01", AttendeeRole.YOUTH_LEADER),
+            createAttendee(2L, "1996-03-01", AttendeeRole.YOUTH_LEADER),
+            createAttendee(6L, "1990-03-01", AttendeeRole.YOUTH_LEADER),
+            createAttendee(3L, "2005-05-01", AttendeeRole.YOUTH_LEADER),
+            createAttendee(4L, "2011-05-01", AttendeeRole.YOUTH),
+            createAttendee(5L, "2022-05-01", AttendeeRole.YOUTH),
         )
 
         val result = pdfHelper.getOptimizedLeaderAndAttendees(allAttendees, LocalDate.of(2023, 5,5))
@@ -41,7 +41,84 @@ class PDFHelperTest {
         Assertions.assertThat(attendees.map { it.id }).isEqualTo(listOf(6L, 1L, 2L))
     }
 
-    fun createAttendee(id: Long, birthday: String): Attendee {
+    @Test
+    fun toOldLeader() {
+      val eventDate = LocalDate.of(2023, 5,5)
+      val allAttendees = listOf(
+        createAttendeeAge(1L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(2L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(3L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(4L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(5L, 35, eventDate, AttendeeRole.YOUTH_LEADER),
+        createAttendeeAge(6L, 35, eventDate, AttendeeRole.YOUTH_LEADER),
+        createAttendeeAge(7L, 35, eventDate, AttendeeRole.YOUTH_LEADER)
+      )
+
+        val result = pdfHelper.getOptimizedLeaderAndAttendees(allAttendees, eventDate)
+        val leader = result.first
+        Assertions.assertThat(leader.map { it.id }).isEqualTo(listOf(5L, 6L, 7L))
+
+        val attendees = result.first
+        Assertions.assertThat(attendees.map { it.id }).isEqualTo(listOf(1L, 2L, 3L, 4L))
+    }
+
+    @Test
+    fun toYoungLeader() {
+      val eventDate = LocalDate.of(2023, 5,5)
+      val allAttendees = listOf(
+        createAttendeeAge(1L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(2L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(3L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(4L, 15, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(5L, 17, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(6L, 17, eventDate, AttendeeRole.YOUTH),
+        createAttendeeAge(7L, 17, eventDate, AttendeeRole.YOUTH_LEADER)
+      )
+
+        val result = pdfHelper.getOptimizedLeaderAndAttendees(allAttendees, eventDate)
+        val leader = result.first
+        Assertions.assertThat(leader.map { it.id }).isEqualTo(listOf(1L, 2L, 3L, 4L, 5L, 6L, 7L))
+
+        val attendees = result.first
+        Assertions.assertThat(attendees.map { it.id }).isEqualTo(emptyList<Attendee>())
+    }
+
+    fun createLeaderAtLeast27(count: Int, eventDate: LocalDate): List<Attendee> {
+      var id = 1L
+      var attendees = listOf<Attendee>()
+      for (i in 1..count) {
+        attendees.plus(createAttendeeAge(id, 36, eventDate, AttendeeRole.YOUTH_LEADER))
+        id++
+      }
+      return attendees
+    }
+
+    fun createLeaderBelow27(count: Int, eventDate: LocalDate): List<Attendee> {
+      var id = 1L
+      var attendees = listOf<Attendee>()
+      for (i in 1..count) {
+        attendees.plus(createAttendeeAge(id, 26, eventDate, AttendeeRole.YOUTH_LEADER))
+        id++
+      }
+      return attendees
+    }
+    
+    fun createAttendees(count: Int, eventDate: LocalDate): List<Attendee> {
+      var id = 1L
+      var attendees = listOf<Attendee>()
+      for (i in 1..count) {
+        attendees.plus(createAttendeeAge(id, 16, eventDate, AttendeeRole.YOUTH))
+        id++
+      }
+      return attendees
+    }
+
+    fun createAttendeeAge(id: Long, age: Int, eventDate: LocalDate, role: AttendeeRole): Attendee {
+      val birthday = "${eventDate.year - age}-${eventDate.monthValue}-${eventDate.dayOfMonth-1}"
+      return createAttendee(id, birthday, role)
+    }
+
+    fun createAttendee(id: Long, birthday: String, role: AttendeeRole): Attendee {
         return Attendee(
             id= id,
             firstName= "first",
@@ -50,7 +127,7 @@ class PDFHelperTest {
             food= Food.MEAT,
             tShirtSize= TShirtSize.L,
             additionalInformation= "",
-            role= AttendeeRole.YOUTH,
+            role= role,
             department = Department(4L, "", "", ""),
             code = "",
             specialLeave= false,
