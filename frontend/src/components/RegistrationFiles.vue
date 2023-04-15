41ae42b6ea636ae2cccd25c39d9921cd4331b6bd
Arne Maier <Arne_Maier@gmx.de>
2023-04-15 21:55:21 +0200

    Hide registraiton files until ready

--- frontend/src/components/RegistrationFiles.vue
+++ frontend/src/components/RegistrationFiles.vue
@@ -1,30 +1,46 @@
 <template>
   <v-container>
     <h1>Anmeldeunterlagen</h1>
-    <p>
-      Die hier aufgelisteten Dateien sind für die Anmeldung am Kreiszeltlager.
-      Die Dateien sind mit den angemeldeten Teilnehmerlisten vorbefüllt und
-      sollen so bei der unterschrieben Anmeldung abgegeben werden, damit eine
-      reibungslose Anmeldung gewährleistet werden kann.
-    </p>
-    <FileList :departmentId="departmentId" :departmentName="departmentName" />
-    <p>
-      ⚠️ In einigen PDF Viewern kommt es zu Probleme mit der Anzeige, es
-      funktionieren mit Google Chrome und Adobe Acrobat Reader. ⚠️
-    </p>
-    <div>
-      <YouthPlanDistribution />
-    </div>
-    <div v-if="hasAdministrationRole() && departments">
-      <h2>Anmeldeunterlagen aller Feuerwehren</h2>
-      <div v-for="department in departments" :key="department.id">
-        <h3>Feuerwehr {{ department.name }}</h3>
-        <FileList
-          :departmentId="department.id"
-          :departmentName="department.name"
-        />
-        <br />
+    <div
+      v-if="
+        startDownloadRegistrationFiles &&
+        startDownloadRegistrationFiles.registrationFilesCanBeDownloaded
+      "
+    >
+      <p>
+        Die hier aufgelisteten Dateien sind für die Anmeldung am Kreiszeltlager.
+        Die Dateien sind mit den angemeldeten Teilnehmerlisten vorbefüllt und
+        sollen so bei der unterschrieben Anmeldung abgegeben werden, damit eine
+        reibungslose Anmeldung gewährleistet werden kann.
+      </p>
+      <FileList :departmentId="departmentId" :departmentName="departmentName" />
+      <p>
+        ⚠️ In einigen PDF Viewern kommt es zu Probleme mit der Anzeige, es
+        funktionieren mit Google Chrome und Adobe Acrobat Reader. ⚠️
+      </p>
+      <div>
+        <YouthPlanDistribution />
       </div>
+      <div v-if="hasAdministrationRole() && departments">
+        <h2>Anmeldeunterlagen aller Feuerwehren</h2>
+        <div v-for="department in departments" :key="department.id">
+          <h3>Feuerwehr {{ department.name }}</h3>
+          <FileList
+            :departmentId="department.id"
+            :departmentName="department.name"
+          />
+          <br />
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="
+        startDownloadRegistrationFiles &&
+        !startDownloadRegistrationFiles.registrationFilesCanBeDownloaded
+      "
+    >
+      Die Anmeldeunterlagen sind noch nicht bereit zum Herunterladen. Sie stehen
+      ab {{ localizedStartDate && localizedStartDate }} zur Verfügung.
     </div>
   </v-container>
 </template>
@@ -43,6 +59,11 @@ import {
   getDepartments,
   getMyDepartment,
 } from "../services/department";
+import {
+  getStartDownloadRegistrationFiles,
+  StartDownloadRegistrationFiles,
+} from "../services/settings";
+import { dateLocalized } from "@/helper/displayDate";
 
 @Component({
   components: { FileList, YouthPlanDistribution },
@@ -51,6 +72,8 @@ export default class RegistrationFiles extends Vue {
   departmentId: string = "";
   departmentName: string = "";
   departments?: Department[] = [];
+  startDownloadRegistrationFiles: StartDownloadRegistrationFiles | null = null;
+  localizedStartDate: string | null = null;
 
   hasAdministrationRole = hasAdministrationRole;
   mounted() {
@@ -69,6 +92,15 @@ export default class RegistrationFiles extends Vue {
           (department) => department.id !== this.departmentId
         );
       });
+
+    getStartDownloadRegistrationFiles().then(
+      (startDownloadRegistrationFiles) => {
+        this.startDownloadRegistrationFiles = startDownloadRegistrationFiles;
+        this.localizedStartDate = dateLocalized(
+          startDownloadRegistrationFiles.startDownloadRegistrationFiles
+        );
+      }
+    );
   }
 }
 </script>
