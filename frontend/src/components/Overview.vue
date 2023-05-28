6b9b95755f7f4693945562d9ec2cb1ec828e4044
Arne Maier <Arne_Maier@gmx.de>
2023-05-28 14:09:46 +0200

    Fix page override bug in batch pdf and add loading spinner

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -5,7 +5,12 @@
       <p class="mr-8">
         Hier können alle Lagerausweise heruntergeladen werden.
         <br />
-        <v-btn color="#ffe760" @click="downloadBatchesPDF" small>
+        <v-btn
+          color="#ffe760"
+          @click="downloadBatchesPDF"
+          small
+          :loading="loadingBatches"
+        >
           Herunterladen
           <v-icon right dark> mdi-cloud-download </v-icon>
         </v-btn>
@@ -25,7 +30,12 @@
         Hier kann die Liste der Essen, die nicht Fleisch sind heruntergeladen
         werden.
         <br />
-        <v-btn color="#ffe760" @click="downloadFoodPDF" small>
+        <v-btn
+          color="#ffe760"
+          @click="downloadFoodPDF"
+          small
+          :loading="loadingFood"
+        >
           Herunterladen
           <v-icon right dark> mdi-cloud-download </v-icon>
         </v-btn>
@@ -38,7 +48,12 @@
         Hier kann die Liste der TShirts und Armbänder pro Feuerwehr
         heruntergeladen werden.
         <br />
-        <v-btn color="#ffe760" @click="downloadTShirtsPDF" small>
+        <v-btn
+          color="#ffe760"
+          @click="downloadTShirtsPDF"
+          small
+          :loading="loadingTshirt"
+        >
           Herunterladen
           <v-icon right dark> mdi-cloud-download </v-icon>
         </v-btn>
@@ -51,109 +66,119 @@
         Hier kann eine Liste pro Feuerwehr zur Verteilung der TShirts und
         Armbänder heruntergeladen werden.
         <br />
-        <v-btn color="#ffe760" @click="downloadDepartmentOverview" small>
+        <v-btn
+          color="#ffe760"
+          @click="downloadDepartmentOverview"
+          small
+          :loading="loadingDepartmentOverview"
+        >
           Herunterladen
           <v-icon right dark> mdi-cloud-download </v-icon>
         </v-btn>
       </p>
     </div>
 
-    <h1>Zeltübersicht</h1>
-    <div>
-      <p class="mr-8">
-        Hier stehen wie viele Zelte insgesamt angemeldet wurden.
-      </p>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">Größe (Länge x Breite)</th>
-              <th class="text-left">Anzahl</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>SG 200 (4m x 5,9m)</td>
-              <td>{{ totalTents.sg200 }}</td>
-            </tr>
-            <tr>
-              <td>SG 20 (5m x 4,74m)</td>
-              <td>{{ totalTents.sg20 }}</td>
-            </tr>
-            <tr>
-              <td>SG 30 (6m x 5,64m)</td>
-              <td>{{ totalTents.sg30 }}</td>
-            </tr>
-            <tr>
-              <td>SG 40 (8m x 5,64m)</td>
-              <td>{{ totalTents.sg40 }}</td>
-            </tr>
-            <tr>
-              <td>SG 50 (10m x 5,64m)</td>
-              <td>{{ totalTents.sg50 }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
+    <div v-if="initialLoanding">
+      <v-progress-circular indeterminate :size="24" />
     </div>
-
-    <div class="d-flex align-baseline">
-      <h1 class="mr-4 mb-2">Teilnehmer</h1>
-      <div class="department-count">
-        Gesamt: {{ totalAttendeeCount }} (Anwesend: {{ enteredAttendeesCount }})
+    <div v-if="!initialLoanding">
+      <h1>Zeltübersicht</h1>
+      <div>
+        <p class="mr-8">
+          Hier stehen wie viele Zelte insgesamt angemeldet wurden.
+        </p>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Größe (Länge x Breite)</th>
+                <th class="text-left">Anzahl</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>SG 200 (4m x 5,9m)</td>
+                <td>{{ totalTents.sg200 }}</td>
+              </tr>
+              <tr>
+                <td>SG 20 (5m x 4,74m)</td>
+                <td>{{ totalTents.sg20 }}</td>
+              </tr>
+              <tr>
+                <td>SG 30 (6m x 5,64m)</td>
+                <td>{{ totalTents.sg30 }}</td>
+              </tr>
+              <tr>
+                <td>SG 40 (8m x 5,64m)</td>
+                <td>{{ totalTents.sg40 }}</td>
+              </tr>
+              <tr>
+                <td>SG 50 (10m x 5,64m)</td>
+                <td>{{ totalTents.sg50 }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
       </div>
-    </div>
-    <v-row>
-      <v-col cols="5">
-        <v-text-field
-          prepend-icon="mdi-magnify"
-          v-model="filterInput"
-          label="Filter nach Teilnehmer"
-          class="w"
-        />
-      </v-col>
-    </v-row>
 
-    <div
-      v-for="registration in departmentWithAttendees"
-      :key="registration.department.id"
-      class="indented-1"
-    >
       <div class="d-flex align-baseline">
-        <h2 class="mr-4 my-8">Feuerwehr {{ registration.department.name }}</h2>
+        <h1 class="mr-4 mb-2">Teilnehmer</h1>
         <div class="department-count">
-          Gesamt Teilnehmerzahl:
-          {{
-            registration.youthAttendees.length + registration.youthLeader.length
-          }}
-        </div>
-        <div class="d-flex justify-end align-center flex-grow-1">
-          <v-btn
-            @click="checkinDepartment(registration.department.id)"
-            class="checkin"
-            rounded
-          >
-            ⛺ Teilnehmer {{ registration.department.name }} einchecken
-          </v-btn>
+          Gesamt: {{ totalAttendeeCount }} (Anwesend:
+          {{ enteredAttendeesCount }})
         </div>
       </div>
-      <div class="indented-2">
-        <AttendeesTable
-          headlineText="Jugendliche"
-          :attendees="registration.youthAttendees"
-          :departmentId="registration.department.id"
-          :role="attendeeRoleYouth"
-        />
-        <AttendeesTable
-          headlineText="Jugendleiter"
-          :attendees="registration.youthLeader"
-          :departmentId="registration.department.id"
-          :role="attendeeRoleYouthLeader"
-        />
-        <TentsPreregistration
-          :departmentId="registration.department.id"
-          :short="true"
-        />
+      <v-row>
+        <v-col cols="5">
+          <v-text-field
+            prepend-icon="mdi-magnify"
+            v-model="filterInput"
+            label="Filter nach Teilnehmer"
+            class="w"
+          />
+        </v-col>
+      </v-row>
+
+      <div
+        v-for="registration in departmentWithAttendees"
+        :key="registration.department.id"
+        class="indented-1"
+      >
+        <div class="d-flex align-baseline">
+          <h2 class="mr-4 my-8">
+            Feuerwehr {{ registration.department.name }}
+          </h2>
+          <div class="department-count">
+            Gesamt Teilnehmerzahl:
+            {{
+              registration.youthAttendees.length +
+              registration.youthLeader.length
+            }}
+          </div>
+          <div class="d-flex justify-end align-center flex-grow-1">
+            <v-btn
+              @click="checkinDepartment(registration.department.id)"
+              class="checkin"
+              rounded
+            >
+              ⛺ Teilnehmer {{ registration.department.name }} einchecken
+            </v-btn>
+          </div>
+        </div>
+        <div class="indented-2">
+          <AttendeesTable
+            headlineText="Jugendliche"
+            :attendees="registration.youthAttendees"
+            :departmentId="registration.department.id"
+            :role="attendeeRoleYouth"
+          />
+          <AttendeesTable
+            headlineText="Jugendleiter"
+            :attendees="registration.youthLeader"
+            :departmentId="registration.department.id"
+            :role="attendeeRoleYouthLeader"
+          />
+        </div>
       </div>
     </div>
 
@@ -227,6 +252,11 @@ export default class AttendeesRegistration extends Vue {
   hasAdministrationRole = hasAdministrationRole;
   totalTents: Tents = {} as Tents;
   enterEvent: Event = {} as Event;
+  initialLoanding = true;
+  loadingBatches = false;
+  loadingFood = false;
+  loadingTshirt = false;
+  loadingDepartmentOverview = false;
 
   get departmentWithAttendees(): DepartmentWithAttendees[] {
     return this.departments
@@ -253,23 +283,35 @@ export default class AttendeesRegistration extends Vue {
   }
 
   downloadBatchesPDF() {
-    getBatches().then((fileData) => showFile(fileData.data, fileData.fileName));
+    this.loadingBatches = true;
+    getBatches().then((fileData) => {
+      this.loadingBatches = false;
+      showFile(fileData.data, fileData.fileName);
+    });
   }
 
   downloadFoodPDF() {
-    getFoodPDF().then((fileData) => showFile(fileData.data, fileData.fileName));
+    this.loadingFood = true;
+    getFoodPDF().then((fileData) => {
+      this.loadingFood = false;
+      showFile(fileData.data, fileData.fileName);
+    });
   }
 
   downloadTShirtsPDF() {
-    getTShirtPDF().then((fileData) =>
-      showFile(fileData.data, fileData.fileName)
-    );
+    this.loadingTshirt = true;
+    getTShirtPDF().then((fileData) => {
+      this.loadingTshirt = false;
+      showFile(fileData.data, fileData.fileName);
+    });
   }
 
   downloadDepartmentOverview() {
-    getDepartmentOverview().then((fileData) =>
-      showFile(fileData.data, fileData.fileName)
-    );
+    this.loadingDepartmentOverview = true;
+    getDepartmentOverview().then((fileData) => {
+      this.loadingDepartmentOverview = false;
+      showFile(fileData.data, fileData.fileName);
+    });
   }
 
   get totalAttendeeCount(): number {
@@ -300,6 +342,7 @@ export default class AttendeesRegistration extends Vue {
       (event: Event) => (this.enterEvent = event)
     );
     getTents().then((tentsList) => {
+      this.initialLoanding = false;
       this.totalTents = tentsList.reduce(
         (acc, current) => {
           acc.sg200 = acc.sg200 + current.sg200;
