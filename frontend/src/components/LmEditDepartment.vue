0b4af4f7ca05963babc27bef254fa576e512eb46
Arne Maier <Arne_Maier@gmx.de>
2024-05-27 20:37:32 +0200

    Reformat contact and login

--- frontend/src/components/LmEditDepartment.vue
+++ frontend/src/components/LmEditDepartment.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { hasAdministrationRole, Roles, rolesText as rolesTitle } from '@/services/authentication'
-import { updateDepartment } from '../services/department'
+import { onMounted, ref } from 'vue'
+import { Roles, rolesText as rolesTitle } from '@/services/authentication'
 import type { Department } from '../services/department'
-import { sendRegistrationMail, userForDepartment, updateRole } from '../services/user'
+import { updateDepartment } from '../services/department'
 import type { User } from '../services/user'
+import { updateRole, userForDepartment } from '../services/user'
 import { useToast } from 'vue-toastification'
 import { showErrorToast } from '@/helper/fetch'
 
@@ -18,9 +18,6 @@ const error = ref<boolean>(false)
 const loading = ref<boolean>(false)
 const saved = ref<boolean>(false)
 const roleLoading = ref<boolean>(false)
-const sendingEmail = ref<boolean>(false)
-const emailSent = ref<boolean>(false)
-const dialogOpen = ref<boolean>(false)
 const user = ref<User>({} as User)
 const leaderName = ref<string>(props.department.leaderName)
 const leaderEmail = ref<string>(props.department.leaderEMail)
@@ -31,20 +28,6 @@ const rolesList = ref<{ value: Roles; title: string }[]>([
     title: rolesTitle(Roles.SPECIALIZED_FIELD_DIRECTOR)
   }
 ])
-
-const closeModal = () => {
-  dialogOpen.value = false
-  sendingEmail.value = false
-  emailSent.value = false
-}
-
-const sendRegistrationEmail = () => {
-  sendingEmail.value = true
-  sendRegistrationMail(user.value.id).then(() => {
-    emailSent.value = true
-    sendingEmail.value = false
-  })
-}
 
 const onUpdateDepartment = () => {
   loading.value = true
@@ -102,56 +85,38 @@ onMounted(async () => {
 <template>
   <div v-if="!error">
     <form v-on:submit.prevent="onUpdateDepartment">
-      <v-text-field variant="underlined" v-model="leaderName" label="Jugendwart" required />
-      <v-text-field
-        variant="underlined"
-        type="email"
-        v-model="leaderEmail"
-        label="Jugendwart Email"
-        required
-      />
       <v-container>
-        <v-row justify="end">
-          <v-dialog v-model="dialogOpen" persistent max-width="500">
-            <template v-slot:activator="{ props }" v-if="hasAdministrationRole()">
-              <v-btn rounded color="primary" dark v-bind="props" class="mb-2">
-                Registriernugsmail versenden
-              </v-btn>
-            </template>
-            <v-card>
-              <template v-slot:title>Registriernugsmail versenden</template>
-              <form v-on:submit.prevent="sendRegistrationEmail" class="mx-2">
-                <v-card-text v-if="!emailSent">
-                  Willst du wirklich?
-                  <ol class="ml-6">
-                    <li>Ein neues Passwort für {{ department.name }} setzen</li>
-                    <li>Das neue Passwort an {{ department.leaderEMail }} schicken</li>
-                  </ol>
-                </v-card-text>
-                <v-card-text v-if="emailSent">
-                  <v-icon medium>mdi-check</v-icon> Registrierungsmail erfolgreich versendet
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn rounded @click="closeModal"> Schließen </v-btn>
-                  <v-btn color="primary" v-if="!emailSent" :loading="sendingEmail" type="submit">
-                    Senden
-                  </v-btn>
-                </v-card-actions>
-              </form>
-            </v-card>
-          </v-dialog>
-          <v-btn rounded color="primary" :loading="loading" type="submit" class="ml-3">
-            <span v-if="saved"> <v-icon medium>mdi-check</v-icon> Gespeichert </span>
-            <span v-if="!saved"> Speichern </span>
-          </v-btn>
+        <v-row>
+          <h4>Kontakt</h4>
+        </v-row>
+        <v-row align="center" justify="end" wrap="wrap" style="gap: 20px">
+          <div class="fixed-width">
+            <v-text-field variant="underlined" v-model="leaderName" label="Jugendwart" required />
+          </div>
+          <div class="flex-grow">
+            <v-text-field
+              variant="underlined"
+              type="email"
+              v-model="leaderEmail"
+              label="Jugendwart Email"
+              required
+            />
+          </div>
+          <div>
+            <v-btn type="submit" :loading="loading" variant="text">
+              <v-icon medium class="mr-2"> mdi-content-save</v-icon>
+            </v-btn>
+          </div>
         </v-row>
       </v-container>
     </form>
     <form v-on:submit.prevent="onUpdateRole">
       <v-container v-if="user.role !== 'ADMIN'">
-        <v-row align="center" justify="space-between">
-          <div class="w-33">
+        <v-row>
+          <h4>Login</h4>
+        </v-row>
+        <v-row align="center" justify="space-between" wrap="wrap" style="gap: 20px">
+          <div class="flex-grow">
             <v-text-field
               type="text"
               v-model="user.username"
@@ -162,7 +127,7 @@ onMounted(async () => {
               required
             />
           </div>
-          <div class="w-33">
+          <div class="fixed-width">
             <v-select
               v-model="user.role"
               :items="rolesList"
@@ -173,9 +138,9 @@ onMounted(async () => {
             ></v-select>
           </div>
           <div>
-            <button type="submit" :loading="roleLoading">
+            <v-btn type="submit" :loading="roleLoading" variant="text">
               <v-icon medium class="mr-2"> mdi-content-save </v-icon>
-            </button>
+            </v-btn>
           </div>
         </v-row>
         <v-divider class="mt-16 mb-8 border-opacity-15"></v-divider>
@@ -184,4 +149,12 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fixed-width {
+  flex: 1 1 200px;
+}
+
+.flex-grow {
+  flex: 2 1 200px;
+}
+</style>
