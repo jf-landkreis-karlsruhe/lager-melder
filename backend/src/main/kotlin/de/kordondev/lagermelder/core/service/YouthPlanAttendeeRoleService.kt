486679a71b13334fd6d890ad619e91db284eb44c
Arne Maier <Arne_Maier@gmx.de>
2025-05-10 21:44:37 +0200

    exclude kueche from list

--- backend/src/main/kotlin/de/kordondev/lagermelder/core/service/YouthPlanAttendeeRoleService.kt
+++ backend/src/main/kotlin/de/kordondev/lagermelder/core/service/YouthPlanAttendeeRoleService.kt
@@ -32,6 +32,7 @@ class YouthPlanAttendeeRoleService(
 
     fun getOptimizedLeaderAndAttendeeIds(): List<YouthPlanAttendeeRoleEntry> {
         val undistributedAttendees = attendeeService.getAttendeesWithoutYouthPlanRole()
+            .filter { it.department.name == "Küche" && it.department.headDepartmentName == "LK Karlsruhe" }
         val distributedAttendees = getAll()
         val newDistributed = attendeeRoleHelper.getOptimizedLeaderAndAttendeeIds(
             distributedAttendees,
